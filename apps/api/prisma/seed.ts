884944ab298e6957553d928f52c9cbe952c69b28
kieuphat159 <kieuphat159@gmail.com>
2025-11-25 16:24:47 +0700

    graphql server

---
+++ apps/api/prisma/seed.ts
@@ -0,0 +1,58 @@
+import { PrismaClient } from '@prisma/client';
+import { faker } from '@faker-js/faker';
+
+const prisma = new PrismaClient();
+
+function generteSlug(title: string) {
+    return title.toLowerCase().trim().replace(/ /g, '-').replace(/[^\w-]+/g, '');
+}
+
+async function main() {
+    const users = Array.from({ length: 10}).map(() => ({
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        bio: faker.lorem.sentence(),
+        avatar: faker.image.avatar(),
+    }))
+
+    await prisma.user.createMany({
+        data: users,
+    });
+
+    const posts = Array.from({ length: 40}).map(() => ({
+        title: faker.lorem.sentence(),
+        slug:generteSlug(faker.lorem.sentence()),
+        content: faker.lorem.paragraphs(3),
+        thumbnail: faker.image.urlLoremFlickr(),
+        authorId: faker.number.int({ min: 1, max: 10 }),
+        published: true
+    }))
+    await Promise.all(
+        posts.map(async(post) => {
+            await prisma.post.create({
+                data: {
+                    ...post,
+                    comments: {
+                        createMany: {
+                            data: Array.from({ length: 20 }).map(() => ({
+                                content: faker.lorem.sentences(),
+                                authorId: faker.number.int({ min: 1, max: 10 }),
+                            }))
+                        }
+                    }
+                }
+            });
+        })
+    );
+
+    console.log('Seeding completed.');
+}
+
+main().then(() => {
+    prisma.$disconnect();
+    process.exit(0);
+}).catch((e) => {
+    console.error(e);
+    prisma.$disconnect();
+    process.exit(1);
+});
--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    PrismaModule,
+    PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
