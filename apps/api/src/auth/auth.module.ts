65037da6b72a821b8298f811810eed1340a0a1b5
kieuphat159 <kieuphat159@gmail.com>
2025-12-02 20:04:29 +0700

    sign in feature

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 
 @Module({
-  imports: [PrismaModule],
-  providers: [AuthResolver, AuthService],
+  imports: [JwtModule.registerAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: async (configService: ConfigService) => ({
+      secret: configService.get<string>('JWT_SECRET'),
+      signOptions: {
+        expiresIn: configService.get<string>('JWT_EXPIRES_IN') as any,
+      },
+    })
+  })],
+  providers: [AuthResolver, AuthService, PrismaService],
 })
 export class AuthModule {}
