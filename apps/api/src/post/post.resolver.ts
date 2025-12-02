7283eb92433f19abe3e28c01c18cfe64df5b46dd
kieuphat159 <kieuphat159@gmail.com>
2025-12-03 00:16:03 +0700

    auth

--- apps/api/src/post/post.resolver.ts
+++ apps/api/src/post/post.resolver.ts
@@ -1,15 +1,20 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int, Context } from '@nestjs/graphql';
 import { PostService } from './post.service';
 import { Post } from './entities/post.entity';
 import { CreatePostInput } from './dto/create-post.input';
 import { UpdatePostInput } from './dto/update-post.input';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth/jwt-auth.guard';
 
 @Resolver(() => Post)
 export class PostResolver {
   constructor(private readonly postService: PostService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Query(() => [Post], { name: 'posts' })
-  async findAll() {
+  async findAll(@Context() context: any) {
+    const user = context.req.user;
+    console.log({ user });
     return await this.postService.findAll();
   }
 }
