89c36f57d85798dda636efb26a136e05ccf6192f
kieuphat159 <kieuphat159@gmail.com>
2025-12-04 18:05:57 +0700

    home page ui

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import DesktopNavbar from "@/components/ui/desktopNavbar";
+import Navbar from "@/components/ui/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <DesktopNavbar>
+          <Navbar />
+        </DesktopNavbar>
         {children}
       </body>
     </html>
