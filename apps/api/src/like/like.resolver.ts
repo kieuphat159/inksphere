b366ad0965a073bfc2d09111a62d265b5b8b4ab7
kieuphat159 <kieuphat159@gmail.com>
2026-06-21 23:47:16 +0700

    feat (like): like and unlike post

--- apps/api/src/like/like.resolver.ts
+++ apps/api/src/like/like.resolver.ts
@@ -1,10 +1,45 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
-import { LikeService } from './like.service';
+import { UseGuards } from '@nestjs/common';
+import { Args, Context, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth/jwt-auth.guard';
 import { Like } from './entities/like.entity';
-import { CreateLikeInput } from './dto/create-like.input';
-import { UpdateLikeInput } from './dto/update-like.input';
+import { LikeService } from './like.service';
 
 @Resolver(() => Like)
 export class LikeResolver {
   constructor(private readonly likeService: LikeService) {}
+  
+  @UseGuards(JwtAuthGuard)
+  @Mutation(() => Boolean)
+  async likePost(
+    @Context() context,
+    @Args('postId', { type: () => Int }) postId: number
+  ) {
+    const userId = context.req.user.id;
+    return await this.likeService.likePost({ userId, postId });
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Mutation(() => Boolean)
+  async unlikePost(
+    @Context() context,
+    @Args('postId', { type: () => Int }) postId: number
+  ) {
+    const userId = context.req.user.id;
+    return await this.likeService.unlikePost({ userId, postId });
+  }
+
+  @Query(() => Int)
+  postLikeCount(@Args('postId', { type: () => Int }) postId: number) {
+    return this.likeService.getPostLikeCount(postId);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Mutation(() => Boolean)
+  async userLikedPost(
+    @Context() context,
+    @Args('postId', { type: () => Int }) postId: number
+  ) {
+    const userId = context.req.user.id;
+    return await this.likeService.userLikedPost({ userId, postId });
+  }
 }
--- apps/api/src/like/like.service.ts
+++ apps/api/src/like/like.service.ts
@@ -1,26 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateLikeInput } from './dto/create-like.input';
 import { UpdateLikeInput } from './dto/update-like.input';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class LikeService {
-  create(createLikeInput: CreateLikeInput) {
-    return 'This action adds a new like';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async likePost({ postId, userId }: {  postId: number; userId: any }) {
+    try {
+      return !!(await this.prisma.like.create({
+        data: {
+          postId,
+          userId,
+        },
+      }));
+    } catch (error) {
+      throw new BadRequestException('Failed to like the post');
+    }
   }
 
-  findAll() {
-    return `This action returns all like`;
+  async unlikePost({ userId, postId }: { userId: any; postId: number }) {
+    try {
+      await this.prisma.like.delete({
+        where: {
+          userId_postId: {
+            userId,
+            postId,
+          },
+        },
+      });
+      return true;
+    } catch (error) {
+      throw new BadRequestException('Failed to unlike the post');
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} like`;
+  async getPostLikeCount(postId: number) {
+    return await this.prisma.like.count({
+      where: {
+        postId,
+      },
+    });
   }
 
-  update(id: number, updateLikeInput: UpdateLikeInput) {
-    return `This action updates a #${id} like`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} like`;
+  async userLikedPost({ userId, postId }: { userId: number; postId: number }) {
+    const like = await this.prisma.like.findUnique({
+      where: {
+        userId_postId: {
+          userId,
+          postId,
+        },
+      },
+    });
+    return !!like;
   }
 }
+  
