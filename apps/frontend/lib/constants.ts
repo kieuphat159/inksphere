b01df0c7c6c62a41cfc47ae324323cf368bdaa56
kieuphat159 <kieuphat159@gmail.com>
2026-06-27 10:36:36 +0700

    fix (auth): critical bug in GGOAuth

--- apps/frontend/lib/constants.ts
+++ apps/frontend/lib/constants.ts
@@ -1,3 +1,3 @@
-export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001";
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000";
 
 export const DEFAULT_PAGE_SIZE = 12;
